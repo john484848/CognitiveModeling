81d62d8f6d52d7646af134b7cde918e0b9812588
Jonathan Kloepfer <kloepj@rpi.edu>
2024-02-10 15:15:55 -0500

    More Work

---
+++ HW01/hw01/P5.py
@@ -0,0 +1,39 @@
+import numpy as np
+import pandas as pd 
+import seaborn as sns
+import matplotlib.pyplot as plt
+charachters=["Ronald McDonald", "Mickey Mouse"]
+charachtersp=np.array([0.3,0.7])
+weapons=["Gloves", "Fork and Knife", "Dirty Bib"]
+weaponmp=np.array([0.67,0.13,0.20,0.21,0.29,0.5])
+predicted=[]
+def choseweapon(charindex):
+    return np.random.choice(weapons,p=weaponmp[charindex*3:charindex*3+3])
+def makestr(charindex,weapon):
+    return charachters[charindex]+" - "+weapon
+def convertp(count,numsim):
+    return count/numsim
+vfunc=np.vectorize(choseweapon)
+vfunc2=np.vectorize(makestr)
+vfunc3=np.vectorize(convertp)
+for i in range(len(charachters)):
+    for l in range(3*i,3*i+3):
+        predicted.append((charachters[i]+" - "+ weapons[l%3],weaponmp[l]*charachtersp[i]))
+for i in predicted:
+    print(i[0],i[1])
+globalres=[]
+for i in range(1,10000):
+    res=[]
+    charchoice=np.random.choice(np.arange(charachtersp.size),size=i,p=charachtersp)
+    weaponl=vfunc(charchoice)
+    resultstr=vfunc2(charchoice,weaponl)
+    outcome,counts=np.unique(resultstr,return_counts=True)
+    counts=vfunc3(counts,numsim=i)
+    diff=0
+    for e in range(counts.size):
+        diff+=abs(predicted[e][1]-counts[e])
+    globalres.append((i,diff))
+df=pd.DataFrame(globalres,columns=["Num sims","Probability Diff"])
+sns.set_theme()
+sns.relplot(data=df, x="Num sims", y="Probability Diff", kind="line")
+plt.show()
